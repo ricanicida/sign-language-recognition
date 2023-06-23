0dfe98668118f448a7e698d932c05748ba4a866c
ricanicida <ricakazama@gmail.com>
2023-06-23 15:27:06 +0200

    modificate the cropping system to a post-cropping without necessity of resizing image

--- hand-tracking/handtracker.py
+++ hand-tracking/handtracker.py
@@ -4,6 +4,7 @@ import mediapipe as mp
 import numpy as np
 import os
 from collections import deque
+from itertools import islice
 
 
 class HandTracker():
@@ -19,20 +20,28 @@ class HandTracker():
         self.results=None
         self.left_image_buffer = deque(5*[[]], 5)
         self.right_image_buffer = deque(5*[[]], 5)
-        self.max_box_side = 0
+        self.left_min_box_size_buffer = deque(5*[-1], 5)
+        self.right_min_box_size_buffer = deque(5*[-1], 5)
+        self.left_max_box_size_buffer = deque(5*[-1], 5)
+        self.right_max_box_size_buffer = deque(5*[-1], 5)
 
     def hands_finder(self,imageRGB):
         self.results = self.hands.process(imageRGB)
         return
     
-    def update_max_box_side(self):
-        self.max_box_side = max([len(x) for x in self.left_image_buffer])
+    def _update_max_box_size(self):
+        self.left_max_box_side = max([len(x) for x in self.left_image_buffer])
+        self.right_max_box_side = max([len(x) for x in self.right_image_buffer])
         return
     
-    def square_boxes_position(self, image, draw=True):
+    def _start_end_point(self, cx, cy, distance):
+        start_point = (cx-distance, cy-distance)
+        end_point = (cx+distance, cy+distance)
+        return start_point, end_point
+
+    def square_box(self, image, draw=True):
         if self.results.multi_hand_landmarks:
             hands_type = []
-            position_info = {}
             h,w,c = image.shape
 
             for hand in self.results.multi_handedness:
@@ -50,52 +59,70 @@ class HandTracker():
                         mf_mcp_cx, mf_mcp_cy = int(lm.x * w), int(lm.y * h)
 
                 distance = int(1.25 * np.sqrt((wrist_cx-mf_mcp_cx)**2 + (wrist_cy-mf_mcp_cy)**2)) # 1.25 * Euclidian distance for hand size reference
-                start_point = (mf_mcp_cx-distance, mf_mcp_cy-distance)
-                end_point = (mf_mcp_cx+distance, mf_mcp_cy+distance)
-                if start_point >= (0,0) and end_point <= (w,h) and draw:
-                    cv2.rectangle(image, start_point, end_point, (0,255,0), 2)
+                start_point, end_point = self._start_end_point(mf_mcp_cx, mf_mcp_cy, distance)
+
+                if start_point >= (0,0) and end_point <= (w,h):
+                    distance_to_border = min(mf_mcp_cx, w-mf_mcp_cx, mf_mcp_cy, h-mf_mcp_cy) # min value from mf_mcp to the border 
+                    sp, ep = self._start_end_point(mf_mcp_cx, mf_mcp_cy, distance_to_border)
+                    initial_cropped_image = image[sp[1]:ep[1], sp[0]:ep[0]] # first crop to reduce the buffer required memory
                     hand_label = hands_type[i]
-                    position_info[hand_label] = [start_point, end_point]
-                
+                    min_box_size = 2*distance # minimum square box side length
+                    max_box_size = 2*distance_to_border # maximum square box side length
+                    self.add_to_buffer(initial_cropped_image, min_box_size, max_box_size, hand_label)
+
+                    if len(hands_type) == 1:
+                        if hand_label == 'Left':
+                            self.add_to_buffer([], -1, -1, 'Right')
+                        elif hand_label == 'Right':
+                            self.add_to_buffer([], -1, -1, 'Left')
+
+                    if draw:
+                        cv2.rectangle(image, start_point, end_point, (0,255,0), 2)
+                        
                 i += 1
-            return position_info, image
+        return
         
-        else:
-            return None, image
-    
-    def crop_hand(self, image, position_info, hand):
-        if position_info and hand in position_info.keys():
-            start_point, end_point = position_info[hand]
-            cropped_image = image[start_point[1]:end_point[1], start_point[0]:end_point[0]]
-            return cropped_image
-        else:
-            return []
+    def recrop_hand(self, image, box_size):
+        original_size = len(image)
+        border_width = int((original_size - box_size)/2)
+        cropped_image = image[border_width:original_size-border_width, border_width:original_size-border_width]
+        return cropped_image
         
-    def add_to_buffer(self, image, hand):
+    def add_to_buffer(self, image, min_box_size, max_box_size, hand):
         if hand == 'Left':
             self.left_image_buffer.appendleft(image)
-        else:
+            self.left_min_box_size_buffer.appendleft(min_box_size)
+            self.left_max_box_size_buffer.appendleft(max_box_size)
+        elif hand == 'Right':
             self.right_image_buffer.appendleft(image)
+            self.right_min_box_size_buffer.appendleft(min_box_size)
+            self.right_max_box_size_buffer.appendleft(max_box_size)
 
     def image_averaging(self, hand):
         if hand == 'Left':
             images = self.left_image_buffer
-        else:
+            min_common_box_size = max(self.left_min_box_size_buffer)
+            max_common_box_size = min(self.left_max_box_size_buffer)
+        elif hand == 'Right':
             images = self.right_image_buffer
+            min_common_box_size = max(self.right_min_box_size_buffer)
+            max_common_box_size = min(self.right_max_box_size_buffer)
 
-        if len(images[-1]) > 0:
-            dim = (self.max_box_side, self.max_box_side)
+        if len(images[0]) > 0 and min_common_box_size <= max_common_box_size:
             avg_image = images[0]
-            avg_image = cv2.resize(avg_image, dim, interpolation = cv2.INTER_AREA)
-            for i in range(1, len(images)):
-                image = cv2.resize(images[i], dim, interpolation = cv2.INTER_AREA)
-                if i == 0:
-                    pass
-                else:
+            avg_image = self.recrop_hand(avg_image, min_common_box_size)
+            
+            i = 1
+            for image in list(islice(images,1,len(images))):
+                if len(image) > 0:
+                    image = self.recrop_hand(images[i], min_common_box_size)
                     alpha = 1.0/(i + 1)
                     beta = 1.0 - alpha
                     avg_image = cv2.addWeighted(image, alpha, avg_image, beta, 0.0)
+                    i += 1
             return avg_image
+        else:
+            return []
         
     def save_image(self, image, file_name, folder_path=os.getcwd()):
         cv2.imwrite(os.path.join(folder_path, file_name), image)
@@ -110,25 +137,22 @@ def main():
         success,image = cap.read()
         imageRGB = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
         tracker.hands_finder(imageRGB)
-        position_info, output_image = tracker.square_boxes_position(image)
+        tracker.square_box(image, draw=True)
 
-        left_crop = tracker.crop_hand(image, position_info, 'Left')
-        if len(left_crop) > 0:
-            cv2.imshow("Cropped", left_crop)
-            cv2.waitKey(1000)
-            # tracker.save_image(left_crop, 'left-hand.jpg')
-            tracker.add_to_buffer(left_crop, 'Left')
-            tracker.update_max_box_side()
-            i += 1
         if i == 5:
-            print(tracker.max_box_side)
-            hei = tracker.image_averaging('Left')
-            if len(hei) > 0:
-                cv2.imshow("HEI", hei)
+            hei_left = tracker.image_averaging('Left')
+            hei_right = tracker.image_averaging('Right')
+            if len(hei_left) > 0:
+                cv2.imshow("HEI left", hei_left)
+                cv2.waitKey(0)
+            if len(hei_right) > 0:
+                cv2.imshow("HEI right", hei_right)
                 cv2.waitKey(0)
             break
 
-        cv2.imshow("Video", output_image)
+        i += 1
+
+        cv2.imshow("Video", image)
         cv2.waitKey(200)
         
 
