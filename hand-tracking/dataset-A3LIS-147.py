3565038f3d88f0888d5b827b68bd73be6e25268d
ricanicida <ricakazama@gmail.com>
2024-11-15 17:40:35 -0300

    model training with color background augmented dataset

--- hand-tracking/dataset-A3LIS-147.py
+++ hand-tracking/dataset-A3LIS-147.py
@@ -10,8 +10,8 @@ def main():
 
     # video_folder_path = "D:/Documentos/Polito/Thesis/Datasets/A3LIS-147_italian/trimmed-10/train/"
     # hei_folder_path = "D:/Documentos/Polito/Thesis/Datasets/A3LIS-147_italian/trimmed-10/hei-train-v3/"
-    video_folder_path = "D:/Documentos/Polito/Thesis/Datasets/A3LIS-147_italian/trimmed-10/test/"
-    hei_folder_path = "D:/Documentos/Polito/Thesis/Datasets/A3LIS-147_italian/trimmed-10/hei-test-v3/"
+    video_folder_path = "D:/Documentos/Polito/Thesis/Datasets/A3LIS-147_italian/trimmed-10/augmented/usual_colors/train/"
+    hei_folder_path = "D:/Documentos/Polito/Thesis/Datasets/A3LIS-147_italian/trimmed-10/augmented/usual_colors/hei-train-v1/"
 
     hei_file_extension = 'jpg'
     
@@ -30,38 +30,45 @@ def main():
 
     for video_file_name in os.listdir(video_folder_path):
 
-        hei_file_name = video_file_name.split(' ')[0]
+        hei_file_name = video_file_name.split('.')[0]
         label = hei_file_name.split('_')[1]
 
-        tracker = handtracker.HandTracker(video_fps=25, hei_sampling_rate=12, hei_max_duration=1, hei_overlap=0.25)
-        cap = cv2.VideoCapture(os.path.join(video_folder_path, video_file_name))
-        success = True
-        while success == True:
-            success, image = cap.read()
-            if success:
-                tracker.tracking(image, subpixel_layout='BGR')         
+        file_alread_exists = (os.path.exists(os.path.join(hei_folder_path, 'left', label, (video_file_name + '_Left.' + hei_file_extension))) 
+                              | os.path.exists(os.path.join(hei_folder_path, 'right', label, (video_file_name + '_Right.' + hei_file_extension))))
         
-                hei_left, hei_right = tracker.image_averaging(label=label, save=True,folder_path=hei_folder_path,
-                                                    file_name=hei_file_name, extension=hei_file_extension)
-                
-                if len(hei_left) > 0:
-                    cv2.imshow("HEI left", hei_left)
-                    cv2.waitKey(500)
-                if len(hei_right) > 0:
-                    cv2.imshow("HEI right", hei_right)
-                    cv2.waitKey(500)
-        
-            
-        hei_left, hei_right = tracker.image_averaging(label=label, last_hei_flag=True,save=True,folder_path=hei_folder_path,
-                                            file_name=hei_file_name, extension=hei_file_extension)
-        
-        if len(hei_left) > 0:
-            cv2.imshow("HEI left", hei_left)
-            cv2.waitKey(500)
-        if len(hei_right) > 0:
-            cv2.imshow("HEI right", hei_right)
-            cv2.waitKey(500)
+        if not file_alread_exists:
 
+            tracker = handtracker.HandTracker(video_fps=25, hei_sampling_rate=12, hei_max_duration=4, hei_overlap=0)
+            cap = cv2.VideoCapture(os.path.join(video_folder_path, video_file_name))
+            success = True
+            while success == True:
+                success, image = cap.read()
+                if success:
+                    tracker.tracking(image, subpixel_layout='BGR')         
+            
+                    hei_left, hei_right = tracker.image_averaging(label=label, save=True,folder_path=hei_folder_path,
+                                                        file_name=hei_file_name, extension=hei_file_extension)
+                    
+                    if len(hei_left) > 0:
+                        cv2.imshow("HEI left", hei_left)
+                        cv2.waitKey(500)
+                    if len(hei_right) > 0:
+                        cv2.imshow("HEI right", hei_right)
+                        cv2.waitKey(500)
+            
+                
+            hei_left, hei_right = tracker.image_averaging(label=label, last_hei_flag=True,save=True,folder_path=hei_folder_path,
+                                                file_name=hei_file_name, extension=hei_file_extension)
+            
+            if len(hei_left) > 0:
+                cv2.imshow("HEI left", hei_left)
+                cv2.waitKey(500)
+            if len(hei_right) > 0:
+                cv2.imshow("HEI right", hei_right)
+                cv2.waitKey(500)
+        
+        else:
+            print(f"{hei_file_name} already exists.")
 
     # video_file_name = "fal_acqua (online-video-cutter.com).mp4"
     # hei_file_name = video_file_name.split(' ')[0]
